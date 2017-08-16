933c2e7ff494cb203fd88011c1c7a3d0317a1a2e
Lee Smet <lee.smet@hotmail.com>
2017-08-16 14:34:53 +0200

    User a slightly older firefox version as temporary workaround

--- karma.conf.js
+++ karma.conf.js
@@ -132,17 +132,8 @@ module.exports = function(config) {
         // how many browser should be started simultaneous
         concurrency: Infinity,
 
-        // customLaunchers: {
-        //     Chrome_travis_ci: {
-        //         base: 'Chrome',
-        //         flags: ['--no-sandbox']
-        //     }
-        // },
     };
 
-    // if (process.env.TRAVIS) {
-    //   configuration.browsers = ['Chrome_travis_ci', 'Firefox'];
-    // }
 
     config.set(configuration);
 }
